b61e61879d749d4bb1b05af8ee678d68ecf148a6
마승현 <swirlflag@nexon.co.kr>
2025-11-05 13:04:33 +0900

    test: ssl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,20 @@
 export default defineNuxtConfig({
 	devtools: { enabled: true },
 	modules: ["@pinia/nuxt"],
+	nitro: {
+		logLevel: 4,
+		preset: "node-server",
+		server: {
+			https: {
+				key: "/nexon.com-key.pem",
+				cert: "/nexon.com.pem",
+				minVersion: "TLSv1.2",
+				maxVersion: "TLSv1.3",
+			},
+		},
+	},
+
+	server: {
+		port: 443,
+	},
 });
